5dc52fed2e868b10b1927667a332254b3743f216
kwani6684 <jack0910@naver.com>
2024-02-09 18:00:21 +0900

    fix: 현위치+클릭한곳 마커 {issue: 현위치로 이동하고 지도클릭시 현위치 마커 안 사라짐}

--- src/app/(pages)/map/page.tsx
+++ src/app/(pages)/map/page.tsx
@@ -1,103 +1,98 @@
-'use client';
+'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 declare global {
   interface Window {
-    kakao: any;
+    kakao: any
   }
 }
 
 const MapContainer: React.FC = () => {
+  const [mainMap, setMainMap] = useState<any>()
+  const [currmarker, setcurrmarker] = useState<any>()
 
-  const [map, setMap] = useState<any>();
-  const [marker, setMarker] = useState<any>();
-  
-    useEffect(() => {
-        const kakaoMapScript = document.createElement('script')
-        kakaoMapScript.async = false
-        kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=6d8ac2fb0740657f1e67a9163c8b331b&autoload=false`
-        document.head.appendChild(kakaoMapScript)
-      
-        const onLoadKakaoAPI = () => {
-          window.kakao.maps.load(() => {
-            const container = document.getElementById('map')
-            const options = {
-              center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-              level: 3,
+  useEffect(() => {
+    const kakaoMapScript = document.createElement('script')
+    kakaoMapScript.async = false
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=6d8ac2fb0740657f1e67a9163c8b331b&autoload=false`
+    document.head.appendChild(kakaoMapScript)
+
+    const onLoadKakaoAPI = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map')
+        const options = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        }
+        var map = new window.kakao.maps.Map(container, options)
+        const marker = new window.kakao.maps.Marker({
+          position: map.getCenter(),
+        })
+        
+        window.kakao.maps.event.addListener(
+          map,
+          'click',
+          function (mouseEvent: { latLng: any }) {
+            // 클릭한 위도, 경도 정보 가져오기
+            const latlng = mouseEvent.latLng
+    
+            // 마커 위치를 클릭한 위치
+            marker.setPosition(latlng)
+    
+            let message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, '
+            message += '경도는 ' + latlng.getLng() + ' 입니다'
+    
+            // 클릭한 위치 정보를 표시할 요소에 메시지를 추가.
+            const resultDiv = document.getElementById('clickLatlng')
+            if (resultDiv) {
+              resultDiv.innerHTML = message
             }
-      
+          },
+        )
 
-            var map = new window.kakao.maps.Map(container, options)
-            setMap(map);
-          })
-        }
-      
-      
-        kakaoMapScript.addEventListener('load', onLoadKakaoAPI)
-    }, [])
-    const getPosSuccess = (pos: GeolocationPosition) => {
-      // 현재 위치(위도, 경도) 가져온다.
-      var currentPos = new window.kakao.maps.LatLng(
-        pos.coords.latitude, // 위도
-        pos.coords.longitude // 경도
-      );
-      // 지도를 이동 시킨다.
-      let markerPosition = new window.kakao.maps.LatLng(currentPos); 
-      
-      map.panTo(currentPos);
-      let marker = new window.kakao.maps.Marker({
-        position: markerPosition
-    });setMarker(currentPos);
-  
-      // 기존 마커를 제거하고 새로운 마커를 넣는다.
-      marker.setMap(null);
-      marker.setPosition(currentPos);
-      marker.setMap(map);
-    };
-    const getCurrentPosBtn = () => {
-      navigator.geolocation.getCurrentPosition(
-        getPosSuccess,
-        () => alert("위치 정보를 가져오는데 실패했습니다."),
-        {
-          enableHighAccuracy: true,
-          maximumAge: 30000,
-          timeout: 27000,
-        }
-      );
+        marker.setMap(map)
+        setMainMap(map)
+      })
     }
+    // 지도 클릭 이벤트 핸들러 등록
+    kakaoMapScript.addEventListener('load', onLoadKakaoAPI)
+  }, [])
 
-            const map = new window.kakao.maps.Map(container, options);
-            // 마커 생성
-            const marker = new window.kakao.maps.Marker({ 
-                position: map.getCenter(),
-            }); 
-          // 지도에 마커 표시
-            marker.setMap(map);
+  const getPosSuccess = (pos: GeolocationPosition) => {
+    // 현재 위치(위도, 경도) 가져온다.
+    let currentPos = new window.kakao.maps.LatLng(
+      pos.coords.latitude, // 위도
+      pos.coords.longitude, // 경도
+    )
+    // 지도를 이동 시킨다.
+    let markerPosition = new window.kakao.maps.LatLng(currentPos)
 
-            // 지도 클릭 이벤트 핸들러 등록
-            window.kakao.maps.event.addListener(map, 'click', function(mouseEvent: { latLng: any; }) {
-                // 클릭한 위도, 경도 정보 가져오기
-                const latlng = mouseEvent.latLng; 
-                
-                // 마커 위치를 클릭한 위치
-                marker.setPosition(latlng);
-                
-                let message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-                message += '경도는 ' + latlng.getLng() + ' 입니다';
-                
-                // 클릭한 위치 정보를 표시할 요소에 메시지를 추가.
-                const resultDiv = document.getElementById('clickLatlng'); 
-                if (resultDiv) {
-                    resultDiv.innerHTML = message;
-                }
-            });
-          })
-        }
-      
-        kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
+    mainMap.panTo(currentPos)
+    let currmarker = new window.kakao.maps.Marker({
+      position: markerPosition,
+    })
+    // setcurrmarker(marker)
 
-      }, [])
+    // 기존 마커를 제거하고 새로운 마커를 넣는다.
+    currmarker.setMap(null)
+    currmarker.setPosition(currentPos)
+    currmarker.setMap(mainMap)
+  }
+  const getCurrentPosBtn = () => {
+    navigator.geolocation.getCurrentPosition(
+      getPosSuccess,
+      () => alert('위치 정보를 가져오는데 실패했습니다.'),
+      {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000,
+      },
+    )
+  }
+
+  // const map = new window.kakao.maps.Map(container, options);
+  // // 마커 생성
 
   return (
     <div>
@@ -106,13 +101,10 @@ const MapContainer: React.FC = () => {
 
       {/* 다른 페이지 컨텐츠 */}
       <div onClick={getCurrentPosBtn}>현재 위치</div>
-
-      
-       {/* 클릭한 위치의 위도, 경도 정보를 표시 */}
+      {/* 클릭한 위치의 위도, 경도 정보를 표시 */}
       <div id="clickLatlng"></div>
-
     </div>
-  );
-};
+  )
+}
 
-export default MapContainer;
+export default MapContainer
