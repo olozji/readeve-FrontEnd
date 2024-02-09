f8e96b6a7738f9679002af1b14a42aafad99dd74
kwani6684 <jack0910@naver.com>
2024-02-09 17:28:32 +0900

    feat: 현재위치 기능 구현

--- src/app/(pages)/map/page.tsx
+++ src/app/(pages)/map/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 declare global {
   interface Window {
@@ -9,6 +9,9 @@ declare global {
 }
 
 const MapContainer: React.FC = () => {
+
+  const [map, setMap] = useState<any>();
+  const [marker, setMarker] = useState<any>();
   
     useEffect(() => {
         const kakaoMapScript = document.createElement('script')
@@ -25,17 +28,50 @@ const MapContainer: React.FC = () => {
             }
       
             var map = new window.kakao.maps.Map(container, options)
+            setMap(map);
           })
         }
       
+      
         kakaoMapScript.addEventListener('load', onLoadKakaoAPI)
-      }, [])
+    }, [])
+    const getPosSuccess = (pos: GeolocationPosition) => {
+      // 현재 위치(위도, 경도) 가져온다.
+      var currentPos = new window.kakao.maps.LatLng(
+        pos.coords.latitude, // 위도
+        pos.coords.longitude // 경도
+      );
+      // 지도를 이동 시킨다.
+      let markerPosition = new window.kakao.maps.LatLng(currentPos); 
+      
+      map.panTo(currentPos);
+      let marker = new window.kakao.maps.Marker({
+        position: markerPosition
+    });setMarker(currentPos);
+  
+      // 기존 마커를 제거하고 새로운 마커를 넣는다.
+      marker.setMap(null);
+      marker.setPosition(currentPos);
+      marker.setMap(map);
+    };
+    const getCurrentPosBtn = () => {
+      navigator.geolocation.getCurrentPosition(
+        getPosSuccess,
+        () => alert("위치 정보를 가져오는데 실패했습니다."),
+        {
+          enableHighAccuracy: true,
+          maximumAge: 30000,
+          timeout: 27000,
+        }
+      );
+    }
 
   return (
     <div>
       {/* 페이지 컨텐츠 및 지도를 표시할 컨테이너 */}
       <div id="map" style={{ width: '100%', height: '400px' }}></div>
       {/* 다른 페이지 컨텐츠 */}
+      <div onClick={getCurrentPosBtn}>현재 위치</div>
     </div>
   );
 };
