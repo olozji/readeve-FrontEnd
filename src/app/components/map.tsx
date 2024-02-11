dd867a27f71cf8a36b79387d73ef2e4aea719c98
kwani6684 <jack0910@naver.com>
2024-02-11 23:39:54 +0900

    feat: 직접 핀으로 검색 기능 구현

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,48 +1,69 @@
 /*global kakao */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import MapSearch from './mapSearch';
+import MapSearch from './mapSearch'
 import searchIcon from 'public/images/searchIcon.png'
-import Image from 'next/image';
+import Image from 'next/image'
+import { MapDirectSearch } from './mapDirectSearch'
 
-const AddPlace = (()=>{
+const AddPlace = () => {
   const [InputText, setInputText] = useState('') // 추가할 장소이름 검색
   const [Place, setPlace] = useState('') // 추가할 장소 데이터 설정
+  const [directSearch, setDirectSerach] = useState(false) //초기 상태는 검색창으로 검색
 
-  const onChange = (e:any) => {
+  const onChange = (e: any) => {
     setInputText(e.target.value)
   }
 
-  const handleSubmit = (e:any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault()
     setPlace(InputText)
     setInputText('')
   }
 
   return (
-    <>
-      <form className="inputForm" onSubmit={handleSubmit}>
-        <div className = "addSearchDiv">
-        <div className="searchInputDiv">
-        <input
-              type="text" size={50} defaultValue=""
-              placeholder="장소를 검색하세요"
-              onChange={onChange}
-              value={InputText}/>
-          {/* <Image id="searchIcon" onClick={handleSubmit} src={searchIcon} alt='search'/> */}
-          </div>
-          <div className="buttonDiv">
-          <button type="submit" style={{display:"none"}}>enterKey시 검색할 수 있는 형식상 submit 버튼</button>
-          <button id="searchBtn" onClick={handleSubmit}>검색</button>
-          </div>
-          </div>
-      </form>
-      
-    
-      <MapSearch searchPlace={Place} />
-      {/*검색한 값을 props로 MapSearch 컴포넌트로 보냄*/ }
+      <>
+          <button onClick={() => { setDirectSerach(!directSearch); }}>
+              {directSearch? `직접 핀으로 검색`:`이름으로 검색`}
+          </button>
+      {directSearch ? (
+        <div>
+          <MapDirectSearch></MapDirectSearch>
+        </div>
+      ) : (
+        <div>
+          <form className="inputForm" onSubmit={handleSubmit}>
+            <div className="addSearchDiv">
+              <div className="searchInputDiv">
+                <input
+                  type="text"
+                  size={50}
+                  defaultValue=""
+                  placeholder="장소를 검색하세요"
+                  onChange={onChange}
+                  value={InputText}
+                />
+                {/* <Image id="searchIcon" onClick={handleSubmit} src={searchIcon} alt='search'/> */}
+              </div>
+              <div className="buttonDiv">
+                <button type="submit" style={{ display: 'none' }}>
+                  enterKey시 검색할 수 있는 형식상 submit 버튼
+                </button>
+                <button id="searchBtn" onClick={handleSubmit}>
+                  검색
+                </button>
+              </div>
+            </div>
+          </form>
+          <MapSearch searchPlace={Place} />
+        </div>
+      )}
+      {/* <MapSearch searchPlace={Place} /> */}
+
+      {/*검색한 값을 props로 MapSearch 컴포넌트로 보냄*/}
+      {/* <MapDirectSearch></MapDirectSearch> */}
     </>
   )
-})
+}
 // AddPlaceMap 컴포넌트에 이름으로 검색한 장소 데이터 전달
-export default AddPlace;
+export default AddPlace
