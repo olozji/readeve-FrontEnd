999140d9d4ac4c7d4e42622e2353c30937852ce2
choiceyourj <eunji00126@gmail.com>
2024-02-15 19:35:06 +0900

    feat: 버튼 컴포넌트 생성

---
+++ src/app/components/buttons/button.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+interface ButtonProps {
+    label:string;
+    onClick?:( e:React.MouseEvent<HTMLButtonElement>) => void;
+    disabled?:boolean;
+    outline?:boolean;
+    small?:boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    onClick,
+    disabled,
+    outline,
+    small
+}) => {
+    return (
+        <button
+          type='submit'
+          disabled={disabled}
+          onClick={onClick}
+          className={`
+            relative
+            disabled:opacity-70
+            disabled:cursor-not-allowed
+            rounded-lg
+            hover:opacity-80
+            transition
+            w-full
+            ${outline ? 'bg-white' : 'bg-indigo-500'}
+            ${outline ? 'border-black' : 'border-rose-500'}
+            ${outline ? 'text-black' : 'text-white'}
+            ${small ? 'text-sm' : 'text-md'}          
+            ${small ? 'py-1' : 'py-3'}          
+            ${small ? 'font-lignt' : 'font-semibold'}          
+            ${small ? 'border-[1px]' : 'border-2'}          
+          `}
+        >
+            {label}
+        </button>
+    )
+}
+
+export default Button;
