0e4701ca8b875c9f2b6ba1c00cb5b5e0f895b609
kwani6684 <jack0910@naver.com>
2024-02-20 01:08:19 +0900

    fix: 내 서재 책 중복 수정

--- src/app/components/bookLayout.tsx
+++ src/app/components/bookLayout.tsx
@@ -4,19 +4,27 @@ import { useEffect, useState } from "react";
 export const BookLayout = () => {
 
     const [documents, setDocuments] = useState<any[]>([]);
-
+  
     useEffect(() => {
         const storedData = localStorage.getItem('allDataInfo');
 
         if (storedData) {
-            const parsedData = JSON.parse(storedData);
-            setDocuments(parsedData);
+          const parsedData = JSON.parse(storedData);
+          const onlyBookData = parsedData.filter((data:any,idx:number) => {
+            return (
+            parsedData.findIndex((data1:any) => {
+              return data.book.isbn === data1.book.isbn
+            }) === idx
+          )
+          })
+            setDocuments(onlyBookData);
         }
     }, []);
 
     return (
         <div className="grid grid-cols-3 ">
-            {documents.map((d: any, i: number) => (
+        {documents.map((d: any, i: number) => (
+             
               <Link key={i} href={`/detail/${d.book && d.book.isbn ? d.book.isbn.replace(" ","") : ""}`}>
               <div
                 className={`justify-items-center rounded-lg border-4 border-transparent`}
