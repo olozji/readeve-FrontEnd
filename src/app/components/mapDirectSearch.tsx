d84ba10a9017f2fcdfb0a470ba85fb59d2cf7845
kwani6684 <jack0910@naver.com>
2024-02-19 23:08:21 +0900

    fix: mapDirectSearch 리코일 구현

--- src/app/components/mapDirectSearch.tsx
+++ src/app/components/mapDirectSearch.tsx
@@ -1,4 +1,5 @@
 import { markersState } from '@/store/mapAtoms'
+import { placeState } from '@/store/writeAtoms'
 import { useEffect, useRef, useState } from 'react'
 import { useRecoilState } from 'recoil'
 
@@ -8,15 +9,15 @@ declare global {
   }
 }
 
-export const MapDirectSearch = ({onMarkerClick}:any) => {
+export const MapDirectSearch = ({ onMarkerClick }: any) => {
   const [map, setMap] = useState<any>()
   const [marker, setMarker] = useState<any>()
   const [address, setAddress] = useState('')
 
-  const [markers, setMarkers] = useRecoilState(markersState);
+  const [markers, setMarkers] = useRecoilState(markersState)
+  const [placeInfo, setPlaceInfo] = useRecoilState(placeState)
 
-    
-  //초기값이 false 여서 처음 마운트될때는 실행 안 되는 useEffect인 커스텀 훅 이에요  
+  //초기값이 false 여서 처음 마운트될때는 실행 안 되는 useEffect인 커스텀 훅 이에요
   const useDidMountEffect = (func: any, deps: any) => {
     const didMount = useRef(false)
 
@@ -26,8 +27,8 @@ export const MapDirectSearch = ({onMarkerClick}:any) => {
     }, deps)
   }
 
-    // 1) 카카오맵 불러오기
-    // 올려주신거 그대로 구현하려고 script태그를 app/layout.tsx에 삽입해서 지도 띄웠어요!
+  // 1) 카카오맵 불러오기
+  // 올려주신거 그대로 구현하려고 script태그를 app/layout.tsx에 삽입해서 지도 띄웠어요!
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -50,9 +51,8 @@ export const MapDirectSearch = ({onMarkerClick}:any) => {
           let currMarker = new window.kakao.maps.Marker({
             position: currentPosition,
           })
-            currMarker.setMap(map)
+          currMarker.setMap(map)
         })
-        
       },
       (error) => {
         console.error('Error getting current position:', error)
@@ -62,9 +62,10 @@ export const MapDirectSearch = ({onMarkerClick}:any) => {
 
   // 2) 최초 렌더링 시에는 제외하고 map이 변경되면 실행
   useDidMountEffect(() => {
-    if (map) { 
-      window.kakao.maps.event.addListener( //이 부분이 nextjs에서 useEffect를 사용해 마운트한 카카오맵을 인식 못해서 리스너가 안 달렸는데
-        map,                               //if(map)으로 map 이 null이 아닐때만 실행 하니까 해결 됐어요
+    if (map) {
+      window.kakao.maps.event.addListener(
+        //이 부분이 nextjs에서 useEffect를 사용해 마운트한 카카오맵을 인식 못해서 리스너가 안 달렸는데
+        map, //if(map)으로 map 이 null이 아닐때만 실행 하니까 해결 됐어요
         'click',
         function (mouseEvent: any) {
           // 주소-좌표 변환 객체를 생성합니다
@@ -81,16 +82,23 @@ export const MapDirectSearch = ({onMarkerClick}:any) => {
 
                 // 클릭한 위치 주소를 가져온다.
                 setAddress(addr)
-                onMarkerClick(addr);
+                onMarkerClick(addr)
 
                 const newMarker = {
                   lat: mouseEvent.latLng.getLat(),
                   lng: mouseEvent.latLng.getLng(),
                   address: addr,
-                };
-  
-                setMarkers((prevMarkers:any) => [...prevMarkers, newMarker]);
+                }
 
+                setMarkers((prevMarkers: any) => [...prevMarkers, newMarker])
+                setPlaceInfo({
+                  place_name: null,
+                  id: null,
+                  y: newMarker.lat,
+                  x: newMarker.lng,
+                  road_address_name: addr,
+                  place_url: null,
+                })
                 // 기존 마커를 제거하고 새로운 마커를 넣는다.
                 marker.setMap(null)
                 // 마커를 클릭한 위치에 표시합니다
@@ -104,8 +112,7 @@ export const MapDirectSearch = ({onMarkerClick}:any) => {
     }
   }, [map])
 
-    
- //---------------------현재 위치 가져오기---------------------------//
+  //---------------------현재 위치 가져오기---------------------------//
 
   const getPosSuccess = (pos: GeolocationPosition) => {
     // 현재 위치(위도, 경도) 가져온다.
