1ec668125238b4c849a763a844e987e8105dfbbc
kwani6684 <jack0910@naver.com>
2024-02-23 00:40:03 +0900

    feat:마커& 인포윈도우 상태 설정

--- src/app/components/listItem.tsx
+++ src/app/components/listItem.tsx
@@ -7,10 +7,11 @@ import { useRecoilState } from 'recoil';
 
 interface listItemProps {
   data: any
-  onListItemClick: (place: any) => void;
+  onListItemClick: (place: any, i: number) => void;
+  index:number
 }
 
-const ListItem = ({ data, onListItemClick }: listItemProps) => {
+const ListItem = ({ data, onListItemClick,index }: listItemProps) => {
   
   const [recoilMap] = useRecoilState<any>(mapState)
   const [isHovered, setIsHovered] = useState(false);
@@ -22,7 +23,7 @@ const ListItem = ({ data, onListItemClick }: listItemProps) => {
     className={`relative left-5 top-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow z-50 ${
       isHovered ? 'hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700' : ''
     }`}
-    onClick={() => onListItemClick(data.place)}
+    onClick={() => onListItemClick(data.place,index)}
   >
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
       {data.place.place_name ? data.place.place_name : data.place.address}
