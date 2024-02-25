b32745316bab8adf8c90063cd7c178fa8b4eaa5b
choiceyourj <eunji00126@gmail.com>
2024-02-26 01:51:07 +0900

    feat: footer 컴포넌트 추가

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,45 @@
+const Footer= () => {
+    return (
+      <footer 
+      className="mx-auto text-center px-4 bottom-0 footer p-10 bg-base-200">
+        <div>
+          <p>
+            <a
+              href="#"
+              className="link link-hover"
+              target="_blank"
+              rel="noreferrer noopener nofollow"
+            >
+              읽는곳곳
+            </a>
+          </p>
+        </div>
+  
+        <ul className="flex">
+          <li>
+          </li>
+          <li>
+          </li>
+          <li>
+          </li>
+          <li>
+          </li>
+          <li>
+          </li>
+          <li>
+          </li>
+        </ul>
+  
+        <div className="grid grid-flow-col gap-4">
+        
+        </div>
+  
+        <div>
+          <p>Copyright © 2024 - 읽는곳곳</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
