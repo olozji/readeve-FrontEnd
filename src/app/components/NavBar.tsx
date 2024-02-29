1ac0cc62578e5412ce581466ffd7647b66efcc44
kwani6684 <jack0910@naver.com>
2024-03-01 00:36:43 +0900

    style: 로그인 모달 스타일 수정

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -133,7 +133,7 @@ const NavBar = () => {
         <CustomModal onClose={closeLoginModal} isOpen={true} isLogin={true}>
           <div className="p-[2rem]">
             <div className="font-bold text-2xl">로그인</div>
-            <div className="text-xs text-[#E57C65]">
+            <div className="text-xs text-[#E57C65] py-2">
               <h1>로그인을 하고 모든 기능을 이용해 보세요</h1>
               <h1>필요한 시간은 단 3초!</h1>
             </div>
