277de2facaceb279f851d24326d082fe08c20529
kwani6684 <jack0910@naver.com>
2024-03-06 03:04:43 +0900

    fix:bookLayout 수정

--- src/app/components/bookLayout.tsx
+++ src/app/components/bookLayout.tsx
@@ -1,65 +1,38 @@
 import { dividerClasses } from '@mui/material'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import Image from 'next/image';
-import NotesImg from '/public/images/notesImg.png';
-import { useRecoilState } from 'recoil';
-import { allReviewDataState } from '@/store/writeAtoms';
-import axios from 'axios';
+import Image from 'next/image'
+import NotesImg from '/public/images/notesImg.png'
+import { useRecoilState } from 'recoil'
+import { allReviewDataState } from '@/store/writeAtoms'
 
 interface bookLayoutProps {
   isMain: boolean
-  bookData:any
+  bookData: any
 }
-export const BookLayout = ({ isMain ,bookData}: bookLayoutProps) => {
+export const BookLayout = ({ isMain, bookData }: bookLayoutProps) => {
   const [documents, setDocuments] = useState<any[]>([])
-  const [parsedData, setParsedData] = useState<any[]>([]) 
+  const [parsedData, setParsedData] = useState<any[]>([])
   const [startIdx, setStartIdx] = useState(0)
-  const [allReviewData, setAllReviewData] = useRecoilState<any>(allReviewDataState);
- 
-
+  const [allReviewData, setAllReviewData] =
+    useRecoilState<any>(allReviewDataState)
   const numVisibleBooks = 5
 
-  const [myData, setMyData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.bookeverywhere.site/api/data/all/${bookData}`,
+  useEffect(() => {
+    if (allReviewData && allReviewData.length > 0) {
+      const filteredData = allReviewData.filter(
+        (data: any) => Number(bookData) === data.socialId,
       )
-      const data = response.data.data
-      setMyData(data)
-      console.log(data)
-    } catch (error) {
-      console.error('Error fetching data:', error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [bookData])
-
-  if (isLoading) {
-    return ''
-  }
-
-  useEffect(() => {
-    
-      const onlyBookData = myData.filter((data: any, idx: number) => {
+      const onlyBookData = filteredData.filter((data: any, idx: number) => {
         return (
-          myData.findIndex((data1: any) => {
-            return data.bookRespDto.isbn === data1.bookRespDto.isbn;
+          filteredData.findIndex((data1: any) => {
+            return data.bookRespDto.isbn === data1.bookRespDto.isbn
           }) === idx
-        );
-      });
-      setDocuments(onlyBookData);
-    
-  }, [myData]);
-  
-
+        )
+      })
+      setDocuments(onlyBookData)
+    }
+  }, [allReviewData, bookData])
 
   const handleClickPrev = () => {
     setStartIdx(Math.max(0, startIdx - numVisibleBooks))
@@ -73,20 +46,76 @@ export const BookLayout = ({ isMain ,bookData}: bookLayoutProps) => {
 
   return (
     <div>
-      {isMain ? (
-        <div className="flex justify-between items-center">
-          <div className="p-2 cursor-pointer" onClick={handleClickPrev}>
-              &lt;
+      {documents.length !== 0 && (
+        <div>
+          {isMain ? (
+            <div className="flex justify-between items-center">
+              <div className="p-2 cursor-pointer" onClick={handleClickPrev}>
+                &lt;
+              </div>
+              <div className="grid grid-cols-5 gap-4 justify-center items-center w-[80rem]">
+                {documents
+                  .slice(startIdx, startIdx + numVisibleBooks)
+                  .map((d: any, i: number) => (
+                    <Link
+                      key={i}
+                      href={`/detail/${d.bookRespDto && d.bookRespDto.isbn ? d.bookRespDto.isbn.replace(' ', '') : ''}`}
+                    >
+                      <div className="flex flex-col items-center rounded-lg border-4 border-transparent p-4 cursor-pointer">
+                        <img
+                          src={
+                            d.bookRespDto.thumbnail
+                              ? d.bookRespDto.thumbnail
+                              : 'http://via.placeholder.com/120X150'
+                          }
+                          alt="책 표지"
+                          className="mb-2 rounded-lg w-[10rem]"
+                        />
+                        <div className="flex gap-2 w-[9rem] items-center">
+                          <span className="inline-flex items-center justify-center max-h-10 rounded-lg gap-1 bg-[#E1E1E1] px-3 py-1 text-xs font-medium text-[#5F5F5F]">
+                            <Image
+                              src={NotesImg}
+                              alt={'NotesImg'}
+                              width={10}
+                              height={10}
+                            />
+                            {
+                              allReviewData.filter(
+                                (data: any) =>
+                                  data.bookRespDto.isbn === d.bookRespDto.isbn,
+                              ).length
+                            }{' '}
+                          </span>
+                          <div className="px-1 py-1 text-[#5F5F5F] text-sm">
+                            {d.bookRespDto.title}
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="p-2 cursor-pointer" onClick={handleClickNext}>
+                  &gt;
+                </div>
+              </div>
             </div>
-          <div className="grid grid-cols-5 gap-4 justify-center items-center w-[80rem]">
-            {documents
-              .slice(startIdx, startIdx + numVisibleBooks)
-              .map((d: any, i: number) => (
+          ) : (
+            <div className="grid grid-cols-5 ">
+              {documents.map((d: any, i: number) => (
                 <Link
                   key={i}
                   href={`/detail/${d.bookRespDto && d.bookRespDto.isbn ? d.bookRespDto.isbn.replace(' ', '') : ''}`}
                 >
-                  <div className="flex flex-col items-center rounded-lg border-4 border-transparent p-4 cursor-pointer">
+                  <div
+                    className={`justify-items-center rounded-lg border-4 border-transparent`}
+                    key={i}
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                    }}
+                  >
                     <img
                       src={
                         d.bookRespDto.thumbnail
@@ -94,62 +123,14 @@ export const BookLayout = ({ isMain ,bookData}: bookLayoutProps) => {
                           : 'http://via.placeholder.com/120X150'
                       }
                       alt="책 표지"
-                      className="mb-2 rounded-lg w-[10rem]"
+                      className="mb-2 rounded"
                     />
-                    <div className='flex gap-2 w-[9rem] items-center'>
-                    <span className="inline-flex items-center justify-center max-h-10 rounded-lg gap-1 bg-[#E1E1E1] px-3 py-1 text-xs font-medium text-[#5F5F5F]">
-                    <Image
-                      src={NotesImg}
-                      alt={'NotesImg'}
-                      width={10}
-                      height={10} 
-                    />  
-                    {allReviewData.filter(
-                      (data: any) =>
-                        data.bookRespDto.isbn === d.bookRespDto.isbn
-                    ).length}{' '}
-                  </span>
-                    <div className="px-1 py-1 text-[#5F5F5F] text-sm">{d.bookRespDto.title}</div>
-                  </div>
+                    <div className="p-4">{d.bookRespDto.title}</div>
                   </div>
                 </Link>
               ))}
-          </div>
-          <div className="flex items-center justify-center">
-            <div className="p-2 cursor-pointer" onClick={handleClickNext}>
-              &gt;
             </div>
-          </div>
-        </div>
-      ) : (
-        <div className="grid grid-cols-5 ">
-          {documents.map((d: any, i: number) => (
-            <Link
-              key={i}
-              href={`/detail/${d.bookRespDto && d.bookRespDto.isbn ? d.bookRespDto.isbn.replace(' ', '') : ''}`}
-            >
-              <div
-                className={`justify-items-center rounded-lg border-4 border-transparent`}
-                key={i}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <img
-                  src={
-                    d.bookRespDto.thumbnail
-                      ? d.bookRespDto.thumbnail
-                      : 'http://via.placeholder.com/120X150'
-                  }
-                  alt="책 표지"
-                  className="mb-2 rounded"
-                />
-                <div className="p-4">{d.bookRespDto.title}</div>
-              </div>
-            </Link>
-          ))}
+          )}
         </div>
       )}
     </div>
