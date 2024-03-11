50693e2050cb09bc87d0fe207e6f7fffc071435b
kwani6684 <jack0910@naver.com>
2024-03-11 23:44:40 +0900

    style: se 지원!!!!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   ],
   theme: {
     screens: {
-      sm: { min: '390px', max: '819px' },
+      sm: { min: '350px', max: '819px' },
       md: { min: '820px', max: '1023px' },
       lg: { min: '1080px' },
     },
