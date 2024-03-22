d5d3ba5674eb8b4b3c2823c22d789928b45d019f
kwani6684 <jack0910@naver.com>
2024-03-23 00:13:38 +0900

    fix:mapHeight수정

--- src/app/(pages)/map/[id]/MapView.tsx
+++ src/app/(pages)/map/[id]/MapView.tsx
@@ -449,7 +449,7 @@ const MapView = ({
                 className={`${isFull !== `100vh` ? 'rounded-lg' : ''}`}
                 style={{
                   width: '100%',
-                  height: `${isFull}`,
+                  height: mapHeight,
                   position: 'relative',
                 }}
               >
@@ -464,7 +464,7 @@ const MapView = ({
 
                     <div
                       ref={listContainerRef}
-                      className="absolute lg:scrollBar sm:flex sm:flex-nowrap sm:overflow-x-auto sm:w-[100vw] sm:top-[70%] w-[26vw] bg-[#f9f9f9] sm:h-[20%] h-full px-[2vw] py-[1vw] bg-opacity-80 sm:bg-opacity-0 overflow-y-auto rounded-lg"
+                      className="absolute lg:scrollBar sm:flex sm:flex-nowrap sm:overflow-x-auto sm:w-[100vw] sm:top-[70%] w-[26vw] bg-[#f9f9f9] sm:h-[30%] h-full px-[2vw] py-[1vw] bg-opacity-80 sm:bg-opacity-0 overflow-y-auto rounded-lg"
                       style={{ zIndex: 2 }}
                     >
                       <div className="flex py-2 w-full sm:hidden justify-between text-center font-bold border-b-[1px] border-gray-600 mb-4">
