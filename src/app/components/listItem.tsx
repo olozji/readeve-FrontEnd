602f7a4b2e2b68452275e124ef098a39510b759a
kwani6684 <jack0910@naver.com>
2024-03-22 12:20:47 +0900

    fix: listItem 마진 삭제

--- src/app/components/listItem.tsx
+++ src/app/components/listItem.tsx
@@ -67,7 +67,7 @@ const ListItem = ({
       {isShared ? (
         <div
         className={`relative text-left block pt-6 my-2 sm:w-[94vw] 
-        border rounded-2xl shadow z-50 sm:mx-[3vw] hover:bg-[#E57C65] hover:border-[#e57c65] hover:border-2 hover:text-white 
+        border rounded-2xl shadow z-50 sm:mx-[1vw] hover:bg-[#E57C65] hover:border-[#e57c65] hover:border-2 hover:text-white 
         ${isSelected ? 'bg-[#E57C65] border-[#E57C65] border-2 text-white' : 'bg-white border-gray-200'}
         `}
           onClick={() => onListItemClick(data.pinRespDto, index)}
